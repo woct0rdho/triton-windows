7e95143c16c7f27a61b6be38844eacad4014d6e0
woctordho <woctordho@outlook.com>
2025-03-21 09:54:58 +0800

    Set close_fds depending on the OS when calling ptxas

--- third_party/nvidia/backend/compiler.py
+++ third_party/nvidia/backend/compiler.py
@@ -400,7 +400,9 @@ class CUDABackend(BaseBackend):
             opt_level = ['--opt-level', '0'] if knobs.nvidia.disable_ptxas_opt else []
             ptxas_cmd = [ptxas, *line_info, *fmad, '-v', *opt_level, f'--gpu-name={arch}', fsrc.name, '-o', fbin]
             try:
-                subprocess.run(ptxas_cmd, check=True, close_fds=True, stdout=flog, stderr=flog)
+                # close_fds=True on Windows and False on Linux, see https://github.com/triton-lang/triton/pull/4357
+                # On Windows, both stdout and stderr need to be redirected to flog
+                subprocess.run(ptxas_cmd, check=True, close_fds=True if os.name == 'nt' else False, stdout=flog, stderr=flog)
             except subprocess.CalledProcessError as e:
                 with open(flog.name) as log_file:
                     log = log_file.read()
