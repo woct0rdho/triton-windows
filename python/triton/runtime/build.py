32141329cf67e0b4d2b20cd4d572cc31652fa978
woctordho <woctordho@outlook.com>
2025-04-10 16:22:44 +0800

    Find MSVC and Windows SDK according to environment variables

--- python/triton/runtime/build.py
+++ python/triton/runtime/build.py
@@ -19,8 +19,12 @@ if os.name == "nt":
     from triton.windows_utils import find_msvc_winsdk, find_python
 
 
+@functools.lru_cache
 def get_cc():
     cc = os.environ.get("CC")
+    if cc is None:
+        # Find and check MSVC and Windows SDK from environment variables set by Launch-VsDevShell.ps1 or VsDevCmd.bat
+        cc, _, _ = find_msvc_winsdk(env_only=True)
     if cc is None:
         # Bundled TinyCC
         cc = os.path.join(sysconfig.get_paths()["platlib"], "triton", "runtime", "tcc", "tcc.exe")
@@ -83,7 +87,7 @@ def _build(name: str, src: str, srcdir: str, library_dirs: list[str], include_di
     if os.name == "nt":
         library_dirs += find_python()
     if is_msvc(cc):
-        msvc_winsdk_inc_dirs, msvc_winsdk_lib_dirs = find_msvc_winsdk()
+        _, msvc_winsdk_inc_dirs, msvc_winsdk_lib_dirs = find_msvc_winsdk()
         include_dirs += msvc_winsdk_inc_dirs
         library_dirs += msvc_winsdk_lib_dirs
     cc_cmd = _cc_cmd(cc, src, so, include_dirs, library_dirs, libraries, ccflags)
