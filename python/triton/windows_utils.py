503373ae0333c1d2e7bbb3e5979fbe5932da31f4
woctordho <woctordho@outlook.com>
2025-04-25 10:00:04 +0800

    Add Python library in _build; support Python 3.13t

--- python/triton/windows_utils.py
+++ python/triton/windows_utils.py
@@ -304,6 +304,8 @@ def find_msvc_winsdk(
 @functools.lru_cache
 def find_python() -> list[str]:
     version = sysconfig.get_python_version().replace(".", "")
+    if sysconfig.get_config_var("Py_GIL_DISABLED"):
+        version += "t"
     for python_base_path in [
         sys.exec_prefix,
         sys.base_exec_prefix,
