ec3c2694cc2028c703cd738753a1c16fb5b6bb10
woctordho <woctordho@outlook.com>
2025-08-10 08:46:31 +0800

    pre-commit

--- python/test/unit/language/test_matmul.py
+++ python/test/unit/language/test_matmul.py
@@ -89,11 +89,10 @@ def get_src_element_ty_size(dtype_str):
 
 @pytest.mark.parametrize("dtype_src_str", ["float32", "tensorfloat32", "float16", "float8e5", "float64"])
 @pytest.mark.parametrize("dtype_dst_str", ["float32", "float16", "float64"])
-@pytest.mark.parametrize("BLOCK_M, BLOCK_N, BLOCK_K, NUM_STAGES", [(128, 128, 16, 4), (64, 128, 32, 4), (32, 32, 32, 4),
 # Skip 256x32@32x128 because it's too large on GPU with max_shared_mem = 101376
-                                                                   # (256, 128, 32, 4), (64, 512, 32, 2),
-                                                                   (64, 512, 32, 2),
-                                                                   (512, 64, 32, 2), (64, 16, 64, 4)])
+@pytest.mark.parametrize("BLOCK_M, BLOCK_N, BLOCK_K, NUM_STAGES", [(128, 128, 16, 4), (64, 128, 32, 4), (32, 32, 32, 4),
+                                                                   # (256, 128, 32, 4),
+                                                                   (64, 512, 32, 2), (512, 64, 32, 2), (64, 16, 64, 4)])
 @pytest.mark.parametrize("NUM_CTAS", [1, 2])
 @pytest.mark.parametrize("NUM_WARPS", [4, 8])
 @pytest.mark.parametrize("EPILOGUE_SUBTILE", [True, False])
